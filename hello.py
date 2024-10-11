6c460c6659c47f6bdbff4d92cb9376bae2d21de6
Olivier Choquet <olivier.choquet@vinci.be>
2024-10-11 09:52:44 +0200

    init repo

---
+++ hello.py
@@ -0,0 +1,16 @@
+
+# Minimal application flask
+#
+# Get started :
+# flask --app hello run
+# * Serving Flask app 'hello'
+# * Running on http://127.0.0.1:5000 (Press CTRL+C to quit)
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
